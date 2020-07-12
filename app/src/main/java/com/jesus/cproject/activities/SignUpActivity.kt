5ac7e4f31e814d44fed18c9f1d3bdae5e272d3fa
TheBlockCode <xxchuchoxd@outlook.com>
2020-07-12 14:52:28 -0500

    go firebase

--- app/src/main/java/com/jesus/cproject/activities/SignUpActivity.kt
+++ app/src/main/java/com/jesus/cproject/activities/SignUpActivity.kt
@@ -1,12 +1,46 @@
 package com.jesus.cproject.activities
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuth.getInstance
 import com.jesus.cproject.R
+import com.jesus.cproject.toast
+
 
 class SignUpActivity : AppCompatActivity() {
+    private val mAuth: FirebaseAuth by lazy { getInstance() }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        val currentUser = mAuth.currentUser
+        if (currentUser != null) {
+            toast("User is NOT logged in")
+            createAccount("yisus2@gmail.com","123456789")
+        } else {
+            toast("user IS logged in")
+
+        }
+
+    }
+
+    private fun createAccount(email: String, password: String) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(
+                this
+            ) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    toast("createUserWithEmail:success")
+                    val user = mAuth.currentUser
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    toast("createUserWithEmail:failure")
+
+                }
+            }
     }
 }
