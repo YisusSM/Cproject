ad76def7453c31390cec89d86ccac4ed0bc52f7e
TheBlockCode <xxchuchoxd@outlook.com>
2020-07-12 17:23:36 -0500

    refactor

--- app/src/main/java/com/jesus/cproject/activities/SignUpActivity.kt
+++ app/src/main/java/com/jesus/cproject/activities/SignUpActivity.kt
@@ -1,12 +1,16 @@
 package com.jesus.cproject.activities
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuth.getInstance
 import com.jesus.cproject.R
+import com.jesus.cproject.activities.login.LoginActivity
+import com.jesus.cproject.goToActivity
 import com.jesus.cproject.toast
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 
 class SignUpActivity : AppCompatActivity() {
@@ -14,33 +18,43 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-
-        val currentUser = mAuth.currentUser
-        if (currentUser != null) {
-            toast("User is NOT logged in")
-            createAccount("yisus2@gmail.com","123456789")
-        } else {
-            toast("user IS logged in")
-
+        btnGoLogin.setOnClickListener {
+            goToActivity<LoginActivity> {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
         }
-
+        btnSignUp.setOnClickListener {
+            val email = editTextEmail.text.toString()
+            val password = editTextPassword.text.toString()
+            if (isValidEmailPassword(email, password)) {
+                signUpByEmail(email, password)
+            } else {
+                toast("Confirma si la contraseña es correcta")
+            }
+        }
     }
 
-    private fun createAccount(email: String, password: String) {
-        mAuth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(
-                this
-            ) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    toast("createUserWithEmail:success")
-                    val user = mAuth.currentUser
+    private fun signUpByEmail(email: String, password: String) {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(
+            this
+        ) { task ->
+            if (task.isSuccessful) {
+                // Sign in success, update UI with the signed-in user's information
+                toast("Un correo se te ha enviado. porfavor confirma.")
 
-                } else {
-                    // If sign in fails, display a message to the user.
-                    toast("createUserWithEmail:failure")
+            } else {
+                // If sign in fails, display a message to the user.
+                toast("Ocurrio un error inesperado. porfavor intentelo de nuevo.")
 
-                }
             }
+
+
+        }
+    }
+
+    private fun isValidEmailPassword(email: String, password: String): Boolean {
+        return !email.isNullOrEmpty() &&
+                !password.isNullOrEmpty() &&
+                password == editTextConfirmPassword.text.toString()
     }
 }
