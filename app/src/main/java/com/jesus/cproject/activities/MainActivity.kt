a42e6190d5767c7ffaff1ccf903aedc581bf97db
TheBlockCode <xxchuchoxd@outlook.com>
2020-07-12 13:50:39 -0500

    login and signUp design

--- app/src/main/java/com/jesus/cproject/activities/MainActivity.kt
+++ app/src/main/java/com/jesus/cproject/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.jesus.cproject
+package com.jesus.cproject.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jesus.cproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
