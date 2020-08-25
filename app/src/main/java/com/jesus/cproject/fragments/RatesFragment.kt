6a1c1c13d501c32d2e848ae7aa35c9e041e5f435
TheBlockCode <xxchuchoxd@outlook.com>
2020-08-24 23:51:00 -0500

    rates fragment

--- app/src/main/java/com/jesus/cproject/fragments/RatesFragment.kt
+++ app/src/main/java/com/jesus/cproject/fragments/RatesFragment.kt
@@ -5,16 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.jesus.cproject.R
+import com.jesus.cproject.adapters.RatesAdapter
+import com.jesus.cproject.dialogues.RateDialog
+import com.jesus.cproject.models.Rate
+import kotlinx.android.synthetic.main.fragment_rates.view.*
 
 
 class RatesFragment : Fragment() {
 
+    private lateinit var _view: View
+
+    private lateinit var adapter: RatesAdapter
+    private val ratesList: ArrayList<Rate> = ArrayList()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_rates, container, false)
+        _view = inflater.inflate(R.layout.fragment_rates, container, false)
+        setUpFab()
+        setUpRecyclerView()
+        return _view
     }
 
+    private fun setUpRecyclerView() {
+        val layoutManager = LinearLayoutManager(context)
+        adapter = RatesAdapter(ratesList)
+        _view.recyclerView.setHasFixedSize(true)
+        _view.recyclerView.layoutManager = layoutManager
+        _view.recyclerView.itemAnimator = DefaultItemAnimator()
+        _view.recyclerView.adapter = adapter
+    }
+
+    private fun setUpFab() {
+        _view.fabRating.setOnClickListener { RateDialog().show(fragmentManager!!, "") }
+
+    }
 }
